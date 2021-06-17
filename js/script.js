aa5f3a5c2311e5111d22538edd03cdafa8a2b518
Novik-Artem <81442713+Novik-Artem@users.noreply.github.com>
2021-06-17 13:46:51 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.menu__icon').click(function(event){
+		$('.menu__icon,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+
+function ibg(){
+let ibg=document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++)
+	{ if(ibg[i].querySelector('img'))
+	{ ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')'; } }
+
+}
+
+ibg();
